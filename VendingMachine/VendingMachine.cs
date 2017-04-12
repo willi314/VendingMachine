971a5f25620fc223493130f2f2465006e2303b7f
willi314 <willi314@muohio.edu>
2017-04-11 20:57:30 -0400

    First VendingMachine test

---
+++ VendingMachine/VendingMachine.cs
@@ -0,0 +1,10 @@
+﻿namespace PillarVendingMachine
+{
+    public class VendingMachine
+    {
+        public string checkDisplay()
+        {
+            return "INSERT COIN";
+        }
+    }
+}
---
+++ VendingMachineTest/VendingMachineTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PillarVendingMachine;
+
+namespace PillarVendingMachineTests
+{
+    [TestClass]
+    public class VendingMachineTest
+    {
+        [TestMethod]
+        public void CheckInitialDisplay()
+        {
+            VendingMachine vendingMachine = new VendingMachine();
+            Assert.AreEqual(vendingMachine.checkDisplay(), "INSERT COIN");
+        }
+    }
+}
